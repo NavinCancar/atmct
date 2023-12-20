75c49e3ced0c2b7417226ff7d6035d123033491c
NavinCancar <nguyenphuonghieu06072002@gmail.com>
2023-12-20 16:53:25 +0700

    Sua loi nho

--- tramxang.php
+++ tramxang.php
@@ -286,7 +286,7 @@
 
 
         <!-- Back to Top -->
-        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+        <a href="#" class="btn btn-lg btn-orange btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
     </div>
 </body>
 </html>
